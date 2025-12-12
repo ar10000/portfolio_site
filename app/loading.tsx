f4d868e0c50bafe8c012b26925ef9c04874baee8
Andrew Ryan <andrewryan763@gmail.com>
2025-12-12 11:21:34 +0000

    Remove 'Brewing something awesome...' banner from loading state

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,7 @@ export default function Loading() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
-        <div className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-purple-500 border-r-transparent mb-4"></div>
-        <p className="text-gray-400">Brewing something awesome...</p>
+        <div className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-purple-500 border-r-transparent"></div>
       </div>
     </div>
   );
